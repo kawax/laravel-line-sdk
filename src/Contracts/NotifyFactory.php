52ef783d32a6f6ed7307dd3e7af9c750be50d2f6
kawax <kawaxbiz@gmail.com>
2020-10-15 10:59:24 +0900

    Update NotifyFactory.php

--- src/Contracts/NotifyFactory.php
+++ src/Contracts/NotifyFactory.php
@@ -2,21 +2,17 @@
 
 namespace Revolution\Line\Contracts;
 
-use GuzzleHttp\Exception\GuzzleException;
-
 interface NotifyFactory
 {
     /**
      * @param  string  $token
      * @return array
-     * @throws GuzzleException
      */
     public function status(string $token): array;
 
     /**
      * @param  string  $token
      * @return array
-     * @throws GuzzleException
      */
     public function revoke(string $token): array;
 }
