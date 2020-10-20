ba15908db6124a7922122ad2415db317bdf24395
kawax <kawaxbiz@gmail.com>
2020-10-20 09:30:58 +0900

    bot

--- src/Facades/Bot.php
+++ src/Facades/Bot.php
@@ -3,21 +3,17 @@
 namespace Revolution\Line\Facades;
 
 use Illuminate\Support\Facades\Facade;
-use LINE\LINEBot\MessageBuilder;
-use LINE\LINEBot\Response;
 use Revolution\Line\Contracts\BotFactory;
-use Revolution\Line\Messaging\BotClient;
-use Revolution\Line\Messaging\ReplyMessage;
 
 /**
- * @method static Response replyMessage($replyToken, MessageBuilder $messageBuilder)
- * @method static Response replyText($replyToken, $text, $extraTexts = null)
- * @method static ReplyMessage reply(string $token)
- * @method static Response pushMessage($to, MessageBuilder $messageBuilder, $notificationDisabled = false, $retryKey = null)
+ * @method static \LINE\LINEBot\Response replyMessage($replyToken, \LINE\LINEBot\MessageBuilder $messageBuilder)
+ * @method static \LINE\LINEBot\Response replyText($replyToken, $text, $extraTexts = null)
+ * @method static \Revolution\Line\Messaging\ReplyMessage reply(string $token)
+ * @method static \LINE\LINEBot\Response pushMessage($to, \LINE\LINEBot\MessageBuilder $messageBuilder, $notificationDisabled = false, $retryKey = null)
  * @method static mixed parseEventRequest($body, $signature, $eventOnly = true)
  *
  * @see \LINE\LINEBot
- * @see BotClient
+ * @see \Revolution\Line\Messaging\BotClient
  */
 class Bot extends Facade
 {
