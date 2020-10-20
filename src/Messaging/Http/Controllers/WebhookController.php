15d45f72036aaee35226dae5a41355216e6e1e82
kawax <kawaxbiz@gmail.com>
2020-10-20 22:00:47 +0900

    Update WebhookController

--- src/Messaging/Http/Controllers/WebhookController.php
+++ src/Messaging/Http/Controllers/WebhookController.php
@@ -4,9 +4,10 @@ namespace Revolution\Line\Messaging\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Routing\Controller;
 use Revolution\Line\Contracts\WebhookHandler;
 
-class WebhookController
+class WebhookController extends Controller
 {
     /**
      * @param  Request  $request
