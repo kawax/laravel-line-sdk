10593a5c29ab6328001d72c111822bc6f0e76e18
kawax <kawaxbiz@gmail.com>
2023-04-06 10:25:59 +0900

    Update Bot.php

--- src/Facades/Bot.php
+++ src/Facades/Bot.php
@@ -5,17 +5,15 @@ namespace Revolution\Line\Facades;
 use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Facade;
-use LINE\LINEBot\MessageBuilder;
-use LINE\LINEBot\Response;
 use Revolution\Line\Contracts\BotFactory;
 use Revolution\Line\Messaging\ReplyMessage;
 
 /**
  * @method static ReplyMessage reply(string $token)
  * @method static Collection parseEvent(Request $request)
- * @method static Response replyMessage(string $replyToken, MessageBuilder $messageBuilder)
- * @method static Response replyText(string $replyToken, string $text, ?array $extraTexts = null)
- * @method static Response pushMessage(string $to, MessageBuilder $messageBuilder, bool $notificationDisabled = false, ?string $retryKey = null, array $customAggregationUnits = [])
+ * @method static \LINE\LINEBot\Response replyMessage(string $replyToken, \LINE\LINEBot\MessageBuilder $messageBuilder)
+ * @method static \LINE\LINEBot\Response replyText(string $replyToken, string $text, ?array $extraTexts = null)
+ * @method static \LINE\LINEBot\Response pushMessage(string $to, \LINE\LINEBot\MessageBuilder $messageBuilder, bool $notificationDisabled = false, ?string $retryKey = null, array $customAggregationUnits = [])
  *
  * @see \LINE\LINEBot
  * @see \Revolution\Line\Messaging\BotClient
