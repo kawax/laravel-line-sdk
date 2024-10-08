a3668f833cc25267db27414aeceb16e980740373
kawax <kawaxbiz@gmail.com>
2024-10-08 09:40:12 +0900

    Update LeaveListener.php

--- stubs/listeners/Line/LeaveListener.php
+++ stubs/listeners/Line/LeaveListener.php
@@ -4,7 +4,9 @@ namespace App\Listeners\Line;
 
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use LINE\Webhook\Model\GroupSource;
 use LINE\Webhook\Model\LeaveEvent;
+use LINE\Webhook\Model\RoomSource;
 
 class LeaveListener
 {
@@ -22,6 +24,12 @@ class LeaveListener
     public function handle(LeaveEvent $event): void
     {
         $source = $event->getSource();
+
+        if ($source instanceof GroupSource) {
+            $id = $source->getGroupId();
+        } elseif ($source instanceof RoomSource) {
+            $id = $source->getRoomId();
+        }
 
         info($source);
     }
