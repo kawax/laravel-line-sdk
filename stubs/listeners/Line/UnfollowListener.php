2fb6242d6dd0333b57d2bf2afdbcc6f64f475614
kawax <kawaxbiz@gmail.com>
2024-10-08 09:40:10 +0900

    Update UnfollowListener.php

--- stubs/listeners/Line/UnfollowListener.php
+++ stubs/listeners/Line/UnfollowListener.php
@@ -5,6 +5,7 @@ namespace App\Listeners\Line;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use LINE\Webhook\Model\UnfollowEvent;
+use LINE\Webhook\Model\UserSource;
 
 class UnfollowListener
 {
@@ -22,6 +23,10 @@ class UnfollowListener
     public function handle(UnfollowEvent $event): void
     {
         $source = $event->getSource();
+
+        if ($source instanceof UserSource) {
+            $id = $source->getUserId();
+        }
 
         info($source);
     }
